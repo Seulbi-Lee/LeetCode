9b1ad5518cd4dec01524ae5d496b71b731d4d1ea
Seulbi-Lee <88129249+Seulbi-Lee@users.noreply.github.com>
2023-08-14 14:16:26 +0900

    Time: 84 ms (77.63%), Space: 59 MB (8.22%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const map = new Map()
+    for (let num of nums){
+        if(map.has(num)) return true
+        else map.set(num)
+    }
+    return false
+};
