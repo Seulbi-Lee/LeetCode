699d4c2978cc3bc45aefae5963089c1088793a73
Seulbi-Lee <88129249+Seulbi-Lee@users.noreply.github.com>
2023-08-25 04:45:13 +0900

    Time: 76 ms (96.93%), Space: 53.7 MB (69.65%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -7,7 +7,7 @@ var productExceptSelf = function(nums) {
     const leftPrd = new Array(leng).fill(1)
     const rightPrd = new Array(leng).fill(1)
     const result = new Array(leng).fill(1)
-
+    
     let leftInit = 1
     for(let i = 0; i < leng; i++){
         leftPrd[i] = leftInit
@@ -24,3 +24,39 @@ var productExceptSelf = function(nums) {
 
     return result
 }
+
+/*
+num = [1,2,3,4]
+
+1, 2, 3, 4
+----------
+1, 1, 1, 1 - init value
+----------
+   1, 1, 1
+      2, 2
+         3
+----------
+1, 1, 2, 6 - leftPrd
+----------
+
+1, 2, 3, 4
+----------
+1, 1, 1, 1 - init value
+----------
+4, 4, 4
+3, 3
+2, 
+----------
+24,12, 4, 1 - rightPrd
+----------
+
+left * right
+----------
+24,12, 8, 6
+----------
+
+excepted number
+----------
+1, 2, 3, 4
+----------
+*/
