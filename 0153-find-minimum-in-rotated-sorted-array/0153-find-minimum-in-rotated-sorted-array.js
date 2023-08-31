545126b12c91591b6f132ac0adefb095f7b4670d
Seulbi-Lee <88129249+Seulbi-Lee@users.noreply.github.com>
2023-09-01 06:24:27 +0900

    Time: 48 ms (84.09%), Space: 41.4 MB (95.55%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -16,34 +16,16 @@ var findMin = function(nums) {
             min = nums[mid]
         }
         
-        // 마지막 위치 값이 기준값 보다 작으면 최소값은 오른쪽에 있게되므로 오른쪽 탐색, 아니면 왼쪽 탐색
-        if(nums[mid] > nums[end]) {
-            start = mid + 1             // 시작점을 기준값의 우측으로 한 칸 이동 
-        }else {
-            end = mid - 1               // 시작점을 기준값의 좌측으로 한 칸 이동
+        // 순행구간 체크
+        if(nums[mid] < nums[end]) {  // 우측 순행중(우측에서 최소값은 mid)
+            end = mid - 1            // 좌측 탐색
+        }else {                      // 좌측에서 순행중
+            start = mid + 1          // 우측 탐색
         }
     }
     
     return min
 };
 
-/*
-Binary Search
-
-s     m     e
-3, 4, 5, 1, 2
-   -  -     -
-   4 > 5 : flase
-   5 > 2 : true -> start = mid + 1
-// start = 1, end = 2, mid = 1, min = 초기값
-
-        s,m e
-3, 4, 5, 1, 2
-      -  -  -
-      5 > 1 : ture -> min = 1
-      1 > 2 : flase -> end = mid - 1
-// start = 1, end = 5, mid = 5, min = 1
-
-*/
 
 
