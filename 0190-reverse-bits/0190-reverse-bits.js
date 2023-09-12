6df838b4508656a156a95b536e53ad62ae4652f6
Seulbi-Lee <88129249+Seulbi-Lee@users.noreply.github.com>
2023-09-13 05:42:39 +0900

    Time: 54 ms (80.42%), Space: 44.5 MB (19.35%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.js
+++ 0190-reverse-bits/0190-reverse-bits.js
@@ -3,21 +3,11 @@
  * @return {number} - a positive integer
  */
 var reverseBits = function(n) {
-    let num = n.toString(2).split('').reverse()
-    let arr = new Array(32).fill(0)
-    let result = parseInt((num.join('')), 2)
-    
-    if(num.length !== arr.length){
-        for(let i in num){
-            arr[i] = num[i]
-        }
-        result = parseInt((arr.join('')), 2)
-    }
-    
-    return result
+    return parseInt(n.toString(2).padStart(32,"0").split('').reverse().join(''), 2)
 };
 
 /*
+padStart(count, "number") : count만큼 number를 앞쪽부터 빈칸에 채움
 .toString(n) : 10진수 > n진수
 parseInt(num, 2) : n진수 > 10진수 
 */
