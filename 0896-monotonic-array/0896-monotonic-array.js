5b74866cc82b4644e14476c937c67b97e1a1d1fc
Seulbi-Lee <88129249+Seulbi-Lee@users.noreply.github.com>
2023-10-01 11:23:01 +0900

    Time: 82 ms (36.26%), Space: 53.4 MB (38.00%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isMonotonic = function(nums) {
+    let increase = true
+    let decrease = true
+    
+    for(let i = 1; i < nums.length; i++){
+        if(nums[i-1] < nums[i]) decrease = false
+        if(nums[i-1] > nums[i]) increase = false
+    }
+    
+    return increase || decrease
+};
